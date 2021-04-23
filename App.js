4d7ced339d9f85697a3fbfc42180a284d1aa4c36
Shifa Somji <shifamsomji@gmail.com>
2021-04-22 18:14:51 -0700

    fixing main display

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Navigation from "./navigation/navigation";
+import { StyleSheet } from 'react-native';
+import SwitchNavigator from "./navigation/switchNavigation";
 
 export default function App() {
   return (
-      <Navigation />
+      <SwitchNavigator />
   );
 }
 
