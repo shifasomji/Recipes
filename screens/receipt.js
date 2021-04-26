c66ba4f81a561e0423716a74bb8cf684ff32d972
Shifa Somji <shifamsomji@gmail.com>
2021-04-26 13:16:22 -0700

    changing colors of buttons

--- screens/receipt.js
+++ screens/receipt.js
@@ -1,26 +1,62 @@
 import React from 'react';
 import { StyleSheet, Text, View, Alert, Button } from 'react-native';
+import * as ImagePicker from 'react-native-image-picker'
 
 import {
   APPTEXTRED,
   APPINPUTVIEW
 } from "../style/constants";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Button
-        title="Upload Receipt from Camera"
-        onPress={() => Alert.alert('Upload your receipt from camera!')}
-        color={APPINPUTVIEW}
-      />
+export default class App extends React.Component {
 
-      <Button
-        title="Upload Receipt from Gallery"
-        onPress={() => Alert.alert('Upload your receipt from photo gallery!')}
-      />
-    </View>
-  );
+  state = {
+    photo: null,
+  }
+
+  handleChoosePhoto = () => {
+    const options = {
+      noData: true,
+    }
+
+    ImagePicker.launchImageLibrary(options, (response) => {
+      console.log(response);
+    })
+  }
+
+  render() {
+    const { photo } = this.state
+    return (
+      <View style={styles.container}>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          {photo && (
+            <Image
+              source={{ uri: photo.uri }}
+              style={{ width: 300, height: 300 }}
+            />
+          )}
+          <Button 
+            title="Upload Receipt from Gallery" 
+            onPress={this.handleChoosePhoto}
+            color={APPINPUTVIEW} />
+        </View>
+
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          {photo && (
+            <Image
+              source={{ uri: photo.uri }}
+              style={{ width: 300, height: 300 }}
+            />
+          )}
+          <Button 
+            title="Upload Receipt from Camera" 
+            onPress={this.handleChoosePhoto}
+            color={APPINPUTVIEW} />
+        </View>
+      </View>
+
+      
+    );
+  }
 }
 
 const styles = StyleSheet.create({
--- screens/recipe.js
+++ screens/recipe.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
+import {
+  APPINPUTVIEW
+} from "../style/constants";
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Button
         title="Choose preferences for today's meal"
         onPress={() => Alert.alert('Preferences')}
+        color={APPINPUTVIEW}
       />
 
       <Button
         title="Find Recipe"
         onPress={() => Alert.alert('Recipes will be displayed')}
+        color={APPINPUTVIEW}
       />
     </View>
   );
