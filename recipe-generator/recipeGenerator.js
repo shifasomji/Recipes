7a052f377b8cc907bcf8918d75cb0bd8cb5d379e
soksamnanglim <soksamnanglim@gmail.com>
2021-05-01 14:42:53 -0700

    fixed references to iinstance variables by adding this.

--- recipe-generator/recipeGenerator.js
+++ recipe-generator/recipeGenerator.js
@@ -1,18 +1,14 @@
 // This module is the main class of the component. It requests information from the Spoonacular API
-import { preferenceObject } from preferenceObject;
-import { recipeList } from recipeList;
-import { recipeListItem } from recipeListItem;
+import { TouchableHighlightBase } from "react-native";
+import { PreferenceObject } from PreferenceObject;
+import { RecipeList } from RecipeList;
+
 /**
  * This is the main class that will generate results for the application and interact between the user interactive interface and the spoonacular api.
  * It will use the parser and the query class to request information from the spoonacular api.
  * 
  */
 class RecipeGenerator {
-  usrprefs;
-  recipeId;
-  recipes; // a list of recipeListItem objects
-  recipeInfo; // a data object storing the recipe information.
-  query;
   /**
    * variables storing the json files
    */
@@ -29,7 +25,10 @@ class RecipeGenerator {
   constructor(userPreferences, recipeID) {
     this.usrprefs = userPreferences;
     this.recipeID = recipeID;
-    this.recipes = new recipeList();
+    this.listJson;
+    this.recipeJson;
+    this.recipes; // a list of recipe list items
+    this.recipeInfo; // a data object storing the recipe information
   }
 
   /**
@@ -50,31 +49,26 @@ class RecipeGenerator {
     // call requestRecipes and store the jsonfile as a variable
     // parse the recipesList into an User interactive interface facing object 
     // return the parsed object.
-    preferences = new preferenceObject(uipreferences);
-    try {
-      preferences.parse();
-    }
-    catch (err) {
-      console.log(err);
-    }
+    preferences = new PreferenceObject(this.uipreferences);
+    if (preferences.errorBool)
 
-    fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?ranking=2&number=5&offset=0&excludeIngredients=${preferences.excludeIngredients}&diet=${preferences.diet}&intolerances=${preferences.intolerances}&includeIngredients=${preferences.includeIngredients}`, {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-key": "API-KEY",
-        "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-      }
-    })
-      .then(response => response.json())
-      .then((data) => {
-        // console.log(data);
-        this.listjson = data;
+      fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?ranking=2&number=5&offset=0&excludeIngredients=${preferences.excludeIngredients}&diet=${preferences.diet}&intolerances=${preferences.intolerances}&includeIngredients=${preferences.includeIngredients}`, {
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-key": "API-KEY",
+          "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+        }
       })
-      .catch(err => {
-        console.error(err);
-      });
+        .then(response => response.json())
+        .then((data) => {
+          // console.log(data);
+          this.listJson = data;
+        })
+        .catch(err => {
+          console.error(err);
+        });
 
-    this.recipes.populateRecipes(listjson);
+    this.recipes = new RecipeList(this.listJson);
 
     return this.recipes;
   }
@@ -104,7 +98,7 @@ class RecipeGenerator {
     })
       .then((response) => response.json())
       .then((data) => {
-        this.recipejson = data;
+        this.recipeJson = data;
       })
       .catch(err => {
         console.error(err);
@@ -121,7 +115,7 @@ class RecipeGenerator {
     // iterate through the elements in the spoonacular recipe information jsonfile
     // parse and add the relevant information to the data object uiRecipe
     // return uiRecipe
-    uiRecipe = new UiRecipe(recipejson);
+    uiRecipe = new UiRecipe(this.recipeJson);
 
     return uiRecipe;
   }
