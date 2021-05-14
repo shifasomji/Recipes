e17545c993477833deb892248ee281312e1165c5
soksamnanglim <soksamnanglim@gmail.com>
2021-05-14 13:37:00 -0700

    main file

---
+++ integration-demo/Main.js
@@ -0,0 +1,5 @@
+const inventoryManager = require('../inventory-manager/CategoryManager');
+const receiptExtractor = require('../receipt-scanner/receiptDataExtractor');
+const receiptCollector = require('../receipt-scanner/receiptFileCollector');
+const recipeGenerator = require('../recipe-generator/RecipeGenerator');
+
